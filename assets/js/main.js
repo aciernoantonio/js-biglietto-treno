c391214fb49b69f7e2c9852e3ac2f86f380386e0
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-24 14:25:38 +0100

    basic problem questions

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//chiedere quanti chilometri si vogliono percorrere
+
+    //calcolo del prezzo in base ai chilometri percorsi
+
+//chiedere quanti anni ha il passeggero
+
+    //sconto del 20% per i minorenni
+
+    //sconto del 40% per gli over 65
+
